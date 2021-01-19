4c0b1adce44e2baa066589aff8a4a920697a2fdd
jmc <jmcreat@naver.com>
2021-01-19 21:31:51 +0900

    Updates

---
+++ precache-manifest.1ce75d02485a2dcdb4921ca8289196c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951939ed6090723f16e3c23c743fc955",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "0fcd0f6ca09195ffe9e1",
+    "url": "/movie_app_2019/static/css/main.9728e8eb.chunk.css"
+  },
+  {
+    "revision": "2baf1e1f5ab767ca97cc",
+    "url": "/movie_app_2019/static/js/2.3033b0ce.chunk.js"
+  },
+  {
+    "revision": "0fcd0f6ca09195ffe9e1",
+    "url": "/movie_app_2019/static/js/main.d8373351.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
